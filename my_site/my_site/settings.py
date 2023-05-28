d82ad8f3c4e1aef965b134e4b633ab9addc46aee
Alex <alex.ter.61660z2@gmail.com>
2023-05-28 14:33:57 +0500

    add VK authorization(need to fix name)

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-EN'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
@@ -123,6 +124,8 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+SOCIAL_AUTH_VK_OAUTH2_EXTRA_DATA = ['photo_big']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -130,7 +133,27 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
+SOCIAL_AUTH_VK_OAUTH2_KEY = '51659087'
+SOCIAL_AUTH_VK_OAUTH2_SECRET = '4N1LteQLAprzERIE4rBa'
+
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'users.pipeline.get_avatar',
+)
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
---
+++ my_site/users/pipeline.py
@@ -0,0 +1,13 @@
+def get_avatar(backend, response, user=None, *args, **kwargs):
+
+    if user is None:
+        return
+
+    if backend.name == 'vk-oauth2':
+        url = response.get('photo_big', '')
+    else:
+        url = None
+
+    if url:
+        user.profile.avatar = url
+        user.profile.save()
